2692d5904c61b14418702a164a74329e5ef451aa
Borges <matheusborges03@live.com>
2023-03-09 18:15:38 -0300

    attJS

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ function calcular(){
     let mensagem = "";
     if (imc < 18.5) {
       mensagem = "Magreza";
-    } else if (imc < 18.5 && imc < 24.9) {
+    } else if (imc > 18.5 && imc < 24.9) {
       mensagem = "Normal";
-    } else if (imc < 25 && imc < 29.9) {
+    } else if (imc > 25 && imc < 29.9) {
       mensagem = "Sobrepeso";
-    } else if (imc < 30 && imc < 34.5) {
+    } else if (imc > 30 && imc < 34.5) {
       mensagem = "Obesidade grau I";
-    } else if (imc < 35 && imc < 39.9) {
+    } else if (imc > 35 && imc < 39.9) {
       mensagem = "Obesidade grau II";
     } else if (imc >= 40) {
       mensagem = "Obesidade grau III";
